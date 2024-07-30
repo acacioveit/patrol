c28781ec2e5c6582b3862cf2bb6722346881b650
Jonathan Ferreira <jonathan@cloudwalk.io>
2024-07-30 17:17:54 -0300

    feat: add support to coverage collector

--- packages/patrol_cli/lib/src/android/android_test_backend.dart
+++ packages/patrol_cli/lib/src/android/android_test_backend.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io' show Process;
 import 'dart:io' as io;
-import 'dart:convert' show utf8;
-
 
 import 'package:adb/adb.dart';
 import 'package:dispose_scope/dispose_scope.dart';
 import 'package:file/file.dart';
+import 'package:path/path.dart' as path;
 import 'package:patrol_cli/src/base/exceptions.dart';
 import 'package:patrol_cli/src/base/logger.dart';
 import 'package:patrol_cli/src/base/process.dart';
@@ -15,7 +15,8 @@ import 'package:patrol_cli/src/devices.dart';
 import 'package:platform/platform.dart';
 import 'package:process/process.dart';
 
-import 'test_monitor.dart';
+import 'collect_coverage.dart';
+import 'log_processor.dart';
 
 /// Provides functionality to build, install, run, and uninstall Android apps.
 ///
@@ -44,7 +45,7 @@ class AndroidTestBackend {
   final DisposeScope _disposeScope;
   final Logger _logger;
 
-  TestMonitor? _testMonitor;
+  CoverageCollector? _testMonitor;
   final Completer<void> _testMonitorCompleter = Completer<void>();
 
   Future<void> build(AndroidAppOptions options) async {
@@ -63,7 +64,7 @@ class AndroidTestBackend {
         workingDirectory: _fs.currentDirectory.childDirectory('android').path,
       )
         ..disposedBy(scope);
-      
+
       process.listenStdOut((l) => _logger.detail('\t: $l')).disposedBy(scope);
       process.listenStdErr((l) => _logger.err('\t$l')).disposedBy(scope);
       exitCode = await process.exitCode;
@@ -108,196 +109,104 @@ class AndroidTestBackend {
   ///
   /// If [interruptible] is true, then no exception is thrown on SIGINT. This is
   /// used for Hot Restart.
-Future<void> execute(
-  AndroidAppOptions options,
-  Device device, {
-  bool interruptible = false,
-}) async {
-  Uri? observatoryUri;
-  final logFile = io.File('/Users/jonathanferreira-mba/Documents/github-public/patrol/packages/patrol/example/adb_log.txt');
-
-  if (!await logFile.parent.exists()) {
-    await logFile.parent.create(recursive: true);
-  }
-
-  final logSink = logFile.openWrite();
-
-  // Limpar o cache do adb logcat
-  await _clearLogcatCache(device);
-
-  final coverageCompleter = Completer<void>();
-  final logcatProcess = await _startLogcat(logSink, device, coverageCompleter);
-
-  await _disposeScope.run((scope) async {
-    final subject = '${options.description} on ${device.description}';
-    final task = _logger.task('Executing tests of $subject');
-
-    final process = await _processManager.start(
-      options.toGradleConnectedTestInvocation(isWindows: _platform.isWindows),
-      runInShell: true,
-      environment: {'ANDROID_SERIAL': device.id},
-      workingDirectory: _fs.currentDirectory.childDirectory('android').path,
-    )
-      ..disposedBy(scope);
-
-    process.listenStdOut((l) {
-      _logger.detail('\t: $l');
-    }).disposedBy(scope);
-
-    process.listenStdErr((l) {
-      const prefix = 'There were failing tests. ';
-      if (l.contains(prefix)) {
-        final msg = l.substring(prefix.length + 2);
-        _logger.err('\t$msg');
-      } else {
-        _logger.detail('\t$l');
-      }
-    }).disposedBy(scope);
-
-    final exitCode = await process.exitCode;
-    if (exitCode == 0) {
-      task.complete('Completed executing $subject');
-    } else if (exitCode != 0 && interruptible) {
-      task.complete('App shut down on request');
-    } else if (exitCode == exitCodeInterrupted) {
-      const cause = 'Gradle test execution interrupted';
-      task.fail('Failed to execute tests of $subject ($cause)');
-      throw Exception(cause);
-    } else {
-      final cause = 'Gradle test execution failed with code $exitCode';
-      task.fail('Failed to execute tests of $subject ($cause)');
-      throw Exception(cause);
-    }
-
-    await logSink.close();
-    logcatProcess?.kill();
-
-    await _testMonitor!.stop();
-
-    if (!_testMonitorCompleter.isCompleted) {
-      await _testMonitorCompleter.future;
-    }
-
-  });
-}
-
-Future<void> uninstall(String appId, Device device) async {
-  _logger.detail('Uninstalling $appId from ${device.name}');
-  await _adb.uninstall(appId, device: device.id);
-  _logger.detail('Uninstalling $appId.test from ${device.name}');
-  await _adb.uninstall('$appId.test', device: device.id);
-}
-
-Future<Process?> _startLogcat(IOSink logSink, Device device, Completer<void> coverageCompleter) async {
-  try {
-    final logcatProcess = await Process.start(
-      'adb',
-      ['-s', device.id, 'logcat'],
+  Future<void> execute(
+    AndroidAppOptions options,
+    Device device, {
+    bool interruptible = false,
+  }) async {
+    final logFilePath = path.join(
+      io.Directory.systemTemp.path,
+      'patrol_${device.id}_${DateTime.now().millisecondsSinceEpoch}.log',
     );
 
-    logcatProcess.stdout.transform(utf8.decoder).listen((data) {
-      logSink.writeln(data);
-      _checkForObservatoryUri(data, device, coverageCompleter);
-    });
-
-    logcatProcess.stderr.transform(utf8.decoder).listen((data) {
-      logSink.writeln(data);
-      _checkForObservatoryUri(data, device, coverageCompleter);
-    });
-
-    return logcatProcess;
-  } catch (e) {
-    _logger.err('Failed to start logcat process: $e');
-    return null;
-  }
-}
-
-Future<void> _clearLogcatCache(Device device) async {
-  try {
-    final clearLogcatProcess = await Process.run(
-      'adb',
-      ['-s', device.id, 'logcat', '-c'],
+    final logProcessor = LogProcessor(
+      device.id,
+      logFilePath,
+      (uri) => _handleObservatoryUri(uri, device),
+      _logger,
     );
-    if (clearLogcatProcess.exitCode != 0) {
-      _logger.err('Failed to clear logcat cache: ${clearLogcatProcess.stderr}');
-    }
-  } catch (e) {
-    _logger.err('Error clearing logcat cache: $e');
-  }
-}
 
-  Future<void> _checkForObservatoryUri(String data, Device device,  Completer<void> coverageCompleter) async {
-    final match = RegExp(r'The Dart VM service is listening on (http://[^\s]+)').firstMatch(data);
-    if (match != null) {
-      final observatoryUri = match.group(1);
-      _logger.info('Observatory URI found: $observatoryUri');
+    _testMonitor = CoverageCollector();
 
-      if (observatoryUri != null) {
-        // Inicie o TestMonitor
-        await _portForwarding(Uri.parse(observatoryUri), device);
-        _testMonitor = TestMonitor(observatoryUri);
-        await _testMonitor!.start().then((_) {
-          _testMonitorCompleter.complete();
-        }).catchError((error) {
-          _logger.err('Error in TestMonitor: $error');
-          // _testMonitorCompleter.completeError(error as );
-        });
+    await logProcessor.start();
 
-        // Colete a cobertura
-        await _collectCoverage(Uri.parse(observatoryUri), coverageCompleter);
-      }
-    }
-  }
+    await _disposeScope.run((scope) async {
+      final subject = '${options.description} on ${device.description}';
+      final task = _logger.task('Executing tests of $subject');
 
-  Future<void> _collectCoverage(Uri observatoryUri, Completer<void> coverageCompleter) async {
-    final coverageDir = io.Directory('coverage');
-    if (!await coverageDir.exists()) {
-      await coverageDir.create(recursive: true);
-    }
-    try {
-      final process = await Process.run(
-        'dart',
-        [
-          'pub',
-          'global',
-          'run',
-          'coverage:collect_coverage',
-          '--uri=$observatoryUri',
-          '-o',
-          'coverage/coverage.json',
-          '--resume-isolates',
-        ],
+      final process = await _processManager.start(
+        options.toGradleConnectedTestInvocation(isWindows: _platform.isWindows),
+        runInShell: true,
+        environment: {'ANDROID_SERIAL': device.id},
+        workingDirectory: _fs.currentDirectory.childDirectory('android').path,
+      )
+        ..disposedBy(scope);
+
+      final stdoutCompleter = Completer<void>();
+      final stderrCompleter = Completer<void>();
+
+      process.stdout
+          .transform(utf8.decoder)
+          .transform(const LineSplitter())
+          .listen(
+        (line) {
+          _logger.detail('\t: $line');
+        },
+        onDone: stdoutCompleter.complete,
       );
 
-      if (process.exitCode != 0) {
-        _logger.err('Failed to collect coverage: ${process.stderr}');
+      process.stderr
+          .transform(utf8.decoder)
+          .transform(const LineSplitter())
+          .listen(
+        (line) {
+          const prefix = 'There were failing tests. ';
+          if (line.contains(prefix)) {
+            final msg = line.substring(prefix.length + 2);
+            _logger.err('\t$msg');
+          } else {
+            _logger.detail('\t$line');
+          }
+        },
+        onDone: stderrCompleter.complete,
+      );
+
+      final exitCode = await process.exitCode;
+      await Future.wait([stdoutCompleter.future, stderrCompleter.future]);
+
+      await logProcessor.stop();
+      await _testMonitor?.stop();
+
+      if (exitCode == 0) {
+        task.complete('Completed executing $subject');
+      } else if (exitCode != 0 && interruptible) {
+        task.complete('App shut down on request');
+      } else if (exitCode == exitCodeInterrupted) {
+        const cause = 'Gradle test execution interrupted';
+        task.fail('Failed to execute tests of $subject ($cause)');
+        throw Exception(cause);
       } else {
-        _logger.info('Coverage collected successfully.');
+        final cause = 'Gradle test execution failed with code $exitCode';
+        task.fail('Failed to execute tests of $subject ($cause)');
+        throw Exception(cause);
       }
-      } catch (e) {
-      _logger.err('Error during coverage collection: $e');
-      }
+    });
   }
 
-  Future<void> _portForwarding(Uri observatoryUri, Device device) async {
-      final processAdbForward = await Process.run(
-        'adb',
-        ['-s', device.id ,'forward', '--remove-all'],
-      );
+  Future<void> uninstall(String appId, Device device) async {
+    _logger.detail('Uninstalling $appId from ${device.name}');
+    await _adb.uninstall(appId, device: device.id);
+    _logger.detail('Uninstalling $appId.test from ${device.name}');
+    await _adb.uninstall('$appId.test', device: device.id);
+  }
 
-      if (processAdbForward.exitCode != 0) {
-        _logger.err('Failed to remove all adb forwards: ${processAdbForward.stderr}');
-      }
-      
-      final port = observatoryUri.port;
-
-      final processAdbForwardPort = await Process.run(
-        'adb',
-        ['-s', device.id, 'forward', 'tcp:$port', 'tcp:$port'],
-      );
-
-      if (processAdbForwardPort.exitCode != 0) {
-        _logger.err('Failed to forward port $port: ${processAdbForwardPort.stderr}');
-      }
+  Future<void> _handleObservatoryUri(String url, Device device) async {
+    _logger.info('Observatory URI found: $url');
+    final observatoryUri = Uri.parse(url);
+    final fromHost = observatoryUri.port;
+    final toDevice = observatoryUri.port;
+    await _adb.forwardPorts(
+        fromHost: fromHost, toDevice: toDevice, device: device.id);
+    await _testMonitor!.start(url);
   }
 }
